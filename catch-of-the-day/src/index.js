fbc105dbdaf1337dfd23e5b02a1637183cfee65e
Barry Ke <lbke2008@gmail.com>
2022-09-12 22:53:12 +0800

    add some starting components;

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import "./css/style.css";
+
+render(<App />, document.querySelector("#main"));
