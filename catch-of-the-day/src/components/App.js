a569153639572448e3eafe997edfceb447ea640c
Barry Ke <lbke2008@gmail.com>
2022-09-14 11:32:07 +0800

    Add Fish, AddFishForm, Header, Inventory, App, NotFound, Router, and StorePicker components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,47 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = (fish) => {
+    console.log("adding a fish!");
+    // take a copy of existing state
+    const fishes = { ...this.state.fishes };
+
+    // add new fish to the fish variable
+    fishes[`fish${Date.now()}`] = fish;
+
+    // set the new fish object to state to overwrite the existing state
+    this.setState({
+      fishes: fishes, // or fishes in ES6
+    });
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" age={100} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
 
-        <Inventory />
         <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,41 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  constructor() {
+    super();
+    console.log("gonna create a component;");
+    // change 'this' to StorePicker
+    // this.goToStore = this.goToStore.bind(this);
+  }
+
+  // set as a property
+  goToStore = (event) => {
+    // stop the form from submitting
+    event.preventDefault();
+
+    // get the text from the input
+    const storeName = this.myInput.current.value;
+
+    // change the page to /store/whatever-they-enter
+    // console.log("going to store");
+    this.props.history.push(`/store/${storeName}`);
+  };
+  // componentDidMount() {
+  //   console.log("Mounted!!");
+  //   console.log(this);
+  // }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store: </h2>
-        <input type="text" required placeholder="Store Name"></input>
+
+        <input
+          ref={this.myInput}
+          type="text"
+          required
+          placeholder="Store Name"
+        ></input>
         <button type="submit"> Visit Store </button>
       </form>
     );
